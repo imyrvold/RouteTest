eccb88d8ad64d2c69ae0d66755be209503cb96a2
Ivan C Myrvold <ivan@advantek.no>
2021-06-03 10:42:28 +0200

    Initial Commit

---
+++ RouteTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RouteTest
+//
+//  Created by Ivan C Myrvold on 03/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
